6078d5ea381d68203ad0c54ff3e5115ac738ed04
tomii-zoo <41190746+tomii-zoo@users.noreply.github.com>
2025-04-22 10:03:27 +0900

    Create dp.py

---
+++ dp.py
@@ -0,0 +1,17 @@
+# memo[n] = fib(n) の形でメモを取る
+memo = {}
+
+# フィボナッチ数列
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = fib(n - 1) + fib(n - 2)
+        return memo[n]
+
+print(fib(900))
