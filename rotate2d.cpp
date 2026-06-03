eabfe81e9e69ccfec49c4db33ba41766e4eb0d50
tomii-zoo <41190746+tomii-zoo@users.noreply.github.com>
2026-06-03 17:38:45 +0900

    Format code for better readability in rotate2d.cpp

--- rotate2d.cpp
+++ rotate2d.cpp
@@ -15,36 +15,36 @@ using std::cos;
 
 class vec2 {
 public:
-  double x;
-  double y;
-  vec2(double x, double y) {
-    this->x = x;
-    this->y = y;
-  }
+	double x;
+	double y;
+	vec2(double x, double y) {
+		this->x = x;
+		this->y = y;
+	}
 };
 
 // 2d rotation
 // x = xcos() - ysin()
 // y = xsin() + ycos()
 tuple<double, double> rotation(float x, float y, float rad) {
-  return {
-    (x * cos(rad)) - (y * sin(rad)), 
-    (x * sin(rad)) + (y * cos(rad))
-  };
+	return {
+		(x * cos(rad)) - (y * sin(rad)),
+		(x * sin(rad)) + (y * cos(rad))
+	};
 } 
 vec2 rotation(vec2 v, float rad) {
-  return vec2(
-	  (v.x * cos(rad)) - (v.y * sin(rad)), 
-	  (v.x * sin(rad)) + (v.y * cos(rad))
-  );
+	return vec2(
+		(v.x * cos(rad)) - (v.y * sin(rad)), 
+		(v.x * sin(rad)) + (v.y * cos(rad))
+	);
 } 
 
 int main() {
-  double x = 1;
-  double y = 0;
-  double euler = 45;
+	double x = 1;
+	double y = 0;
+	double euler = 45;
 
-  cout << "origin: (" << x << ", " << y << ")" << endl;
-  auto [rot_x, rot_y] = rotation(x, y, euler * ToRad);
-  cout << "rotate: (" << rot_x << ", " << rot_y << ")";
+	cout << "origin: (" << x << ", " << y << ")" << endl;
+	auto [rot_x, rot_y] = rotation(x, y, euler * ToRad);
+	cout << "rotate: (" << rot_x << ", " << rot_y << ")";
 }
